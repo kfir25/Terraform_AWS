9d6e46d61e000e751c189a98d1bea4a54c7a92d8
kfir <kfir2251@gmail.com>
2025-04-17 13:13:59 +0300

    changed statefile for s3

--- versions.tf
+++ versions.tf
@@ -8,35 +8,23 @@ terraform {
     }
   }
 
-  # I can use S3 bucket with DynamoDB as backend for statelock
-  backend "local" {
-    path = "terraform.tfstate"
+  # I can use S3 bucket with DynamoDB as backend for statelock or local for test
+#   backend "local" {
+#     path = "terraform.tfstate"
+#   }
+
+
+
+  backend "s3" {
+    bucket         = "kfir-terraform-state-bucket"
+    key            = "test/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    # dynamodb_table = "terraform-locks" # Optional: for state locking
   }
+
 }
 
 provider "aws" {
   region = var.region #"us-east-1"  # You can change the region as per your requirement
   }
-
-
-# provider "aws" {
-#   region                      = "us-east-1"
-#   access_key                  = "test"
-#   secret_key                  = "test"
-#   skip_credentials_validation = true
-#   skip_metadata_api_check     = true
-#   skip_requesting_account_id  = true
-
-#   endpoints {
-#     s3                         = "http://localhost:4566"
-#     sqs                        = "http://localhost:4566"
-#     iam                        = "http://localhost:4566"
-#     ecs                        = "http://localhost:4566"
-#     ec2                        = "http://localhost:4566"
-#     logs                       = "http://localhost:4566"
-#     elasticloadbalancing       = "http://localhost:4566"
-#     sts                        = "http://localhost:4566"
-#     cloudwatch                 = "http://localhost:4566"
-#     ssm                        = "http://localhost:4566"
-#   }
-# }
